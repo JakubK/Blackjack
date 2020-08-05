672a6c2aab32425e96cba85241514f536467dd4d
JakubK <jakubkuniszewski1@gmail.com>
2020-08-05 12:51:08 +0200

    Add switching & game init logic for the buttons present in the page

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,71 @@
-function component() {
-  const element = document.createElement('div');
+const gameMenu:HTMLElement = document.querySelector('.game-menu');
+const gameTable:HTMLElement = document.querySelector('.game-table');
+gameTable.style.display = 'none';
 
-  element.innerHTML = "Hello TS"
+const modeButtons = document.querySelectorAll('.game-menu__btn');
+const singlePlayerBtn = modeButtons[0];
+const multiPlayerBtn = modeButtons[1];
 
-  return element;
+const playersInput:HTMLInputElement = document.querySelector('.game-menu__players');
+let playersCount: number;
+let playersPoints: Array<number>;
+let currentPlayerIndex: number;
+
+const drawCardBtn = document.querySelector('.game-table__draw');
+drawCardBtn.addEventListener('click', () => {
+  drawCard()
+});
+const endTurnBtn = document.querySelector('.game-table__turn');
+endTurnBtn.addEventListener('click', async () => {
+  playersPoints.push(0);
+  if(currentPlayerIndex < playersCount-1)
+    currentPlayerIndex++;
+  else {
+    //end game
+  }
+  await startTurn();
+});
+
+const handsContainer = document.querySelector('.game-table__hands');
+
+const playAgainBtn = document.querySelector('.game-table__repeat');
+playAgainBtn.addEventListener('click', () => {
+  handsContainer.innerHTML = '';
+});
+
+const backToMenuBtn = document.querySelector('.game-table__menu');
+backToMenuBtn.addEventListener('click', () => {
+  gameTable.style.display = 'none';
+  gameMenu.style.display = 'block';
+});
+
+async function startGame() {
+  // let deck = await fetchDeck();
+  // let deckId = deck.deck_id;
+  currentPlayerIndex = 0;
+  startTurn();
 }
 
-document.body.appendChild(component());
+async function startTurn() {
+  console.log('current player id ' + currentPlayerIndex);
+}
+
+async function drawCard() {
+  console.log(currentPlayerIndex + " draws a card");
+}
+
+singlePlayerBtn.addEventListener('click', async () => { 
+  gameMenu.style.display = 'none';
+  gameTable.style.display = 'block';
+  playersCount = 1;
+  playersPoints = [];
+  await startGame();
+});
+
+multiPlayerBtn.addEventListener('click', async () => {
+  gameMenu.style.display = 'none';
+  gameTable.style.display = 'block';
+  playersCount = parseInt(playersInput.value);
+  playersPoints = [];
+  await startGame();
+});
