7c9468ac7127fe7b6fe411a996a791825692ea57
JakubK <jakubkuniszewski1@gmail.com>
2020-08-05 15:10:00 +0200

    Move API calls to a separate class

---
+++ src/API/Request.ts
@@ -0,0 +1,25 @@
+import { Deck } from '../Deck'
+import { Cards } from '../Cards'
+
+class Request {
+
+  async get<T>(url: string): Promise<T> {
+    return await fetch(`https://deckofcardsapi.com/api/deck/${url}`, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+  }
+
+  async fetchDeck(): Promise<Deck>{
+    return await this.get<Deck>('new/shuffle/?deck_count=1');
+  }
+  
+  async fetchCards(deckId: string, count: number): Promise<Cards>{
+    return await this.get<Cards>(`${deckId}/draw/?count=${count}`);
+  }
+}
+
+export default new Request();
